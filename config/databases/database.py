f3ccb48d2d86b96181db3f573a16c02830896fa2
rizovictor <rizo.victor@gmail.com>
2024-09-01 13:46:49 -0500

    Initial project setup

---
+++ config/databases/database.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'permits_db',
+        'USER': 'permits_admin',
+        'PASSWORD': 'Orion_%0405_5040%_noirO',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
